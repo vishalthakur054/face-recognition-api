d0c82a278bf83ccaa56af0a80f262403cab0dcc9
Vishal Thakur <vishalthakur054@gmail.com>
2019-01-20 13:15:32 +0530

    changing start command

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
 app.get('/profile/:id',(req,res) => {profile,handleprofile(req,res,db)})
 app.put('/image',(req,res) => {image.handleImage(req,res,db)});
 app.post('/imageurl',(req,res) => {image.handleAPICall(req,res)});
-app.listen(process.env.PORT || 3000, ()=> {console.log('app is running on ${process.env.PORT}') });
+app.listen(process.env.PORT || 3000, ()=> {console.log(`app is running on ${process.env.PORT}`) });
 
 
 //////////////// we need following end points////////////
