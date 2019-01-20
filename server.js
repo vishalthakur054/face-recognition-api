48272852d1ac79e8ef09cc6360bdacde3314b9f8
Vishal Thakur <vishalthakur054@gmail.com>
2019-01-20 13:02:55 +0530

    setting port dynamic

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
 app.get('/profile/:id',(req,res) => {profile,handleprofile(req,res,db)})
 app.put('/image',(req,res) => {image.handleImage(req,res,db)});
 app.post('/imageurl',(req,res) => {image.handleAPICall(req,res)});
-app.listen(3000, ()=> {console.log('app is running on 3000') });
+app.listen(process.env.PORT || 3000, ()=> {console.log('app is running on ${process.env.PORT}') });
 
 
 //////////////// we need following end points////////////
